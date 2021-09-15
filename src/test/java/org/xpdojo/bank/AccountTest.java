95f396b09baba6742a7178a5ac4de210c3a2d7a6
theDocWho <the.regular.engineer@gmail.com>
2021-09-15 17:26:42 +0530

    Failing test case

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,22 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+
+        //arrange
+        Account account = new Account();
+
+        //act
+        account.deposit(100);
+        //assert (I have money);
+        assertThat(account.balance,is(100));
     }
 }
