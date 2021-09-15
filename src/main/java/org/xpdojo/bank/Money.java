7c9b3fba2721a8c5463c74b3690cfcd9a5468a25
theDocWho <the.regular.engineer@gmail.com>
2021-09-15 18:36:50 +0530

    Modification for Money

--- src/main/java/org/xpdojo/bank/Money.java
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -5,4 +5,17 @@ package org.xpdojo.bank;
  * This class should have no accessor methods.
  */
 public class Money {
+    private final Integer amount;
+    private final String currency;
+
+    public Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+    public int getAmount(){
+        return amount;
+    }
+    public String getCurrency(){
+        return currency;
+    }
 }
