82814632bb3edfdc65d1cca3e5651b8292f9edcc
theDocWho <the.regular.engineer@gmail.com>
2021-09-15 18:36:40 +0530

    Modification for Money

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,18 +1,44 @@
 package org.xpdojo.bank;
 
-public class Account {
-    public Integer balance =0;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
-    public void deposit(int i){
-        this.balance += i;
+public class Account {
+    private final BlockingDeque<Money> balance ;
+
+    public Account(int amount, String currency ){
+        if(amount<0) throw new RuntimeException("Account cannot be opened with -ve balance");
+        balance = new LinkedBlockingDeque<>(1);
+        balance.add(new Money(amount,currency));
     }
 
-    public void withdrawal(int i){
-        if(balance-i>=0){
-            this.balance -= i;
+    public Account(String currency){
+        this(0,currency);
+    }
+
+    public Account(int amount){
+        this(amount,"INR");
+    }
+
+
+    public void deposit(int i) throws Exception{
+        Money money = this.balance.poll();
+        money = new Money(money.getAmount()+i,money.getCurrency() );
+        this.balance.put(money);
+    }
+
+    public void withdrawal(int i)throws Exception{
+        if(this.balance.peek().getAmount()-i>=0){
+            Money money = this.balance.poll();
+            money = new Money(money.getAmount()-i,money.getCurrency() );
+            this.balance.put(money);
         }else{
             throw new RuntimeException("The current balance amount  is lower than withdrawal amount");
         }
 
+    }
+
+    public String checkBalance() throws Exception{
+        return this.balance.peek().getCurrency()+" "+this.balance.peek().getAmount();
     }
 }
